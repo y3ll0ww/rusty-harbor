a46de05cfb3f1859857d9e66b85e96cdcaca4dc3
y3ll0ww <jelle.vangeel@legrand-ext.com>
2025-08-13 15:07:17 +0200

    Update tests

--- rusty_harbor/src/client/tests.rs
+++ rusty_harbor/src/client/tests.rs
@@ -1,26 +1,34 @@
+use dotenv::from_filename;
+
 use crate::{client::HarborClient, request::project, response::project::Project};
 
 #[test]
-fn new_client() {
-    let client = HarborClient::from_env("ROBOT_USER_1", "ROBOT_PASS_1").unwrap();
-    println!("{}", client.username);
-    println!("{}", client.password);
+fn harbor_client_can_be_initialized_with_different_credentials() {
+    // Load the template file, containing "OTHER_USER" and "OTHER_PASS"
+    from_filename(".env.template").ok();
+    
+    // Create the client from (a different) environment file
+    let client = HarborClient::from_env("OTHER_USER", "OTHER_PASS").unwrap();
 
-    let client = HarborClient::default();
-    println!("{}", client.username);
-    println!("{}", client.password);
+    // Assert the values from .env.template are being applied
+    assert_eq!("<OTHER_USERNAME>", client.username);
+    assert_eq!("<OTHER_PASSWORD>", client.password);
 }
 
 #[tokio::test]
-async fn foo_bar() {
+async fn get_projects_from_workspace() {
+    // Initialize a default client (using valid .env credentials)
     let client = HarborClient::default();
 
+    // Create a GET Projects request through the builder
     let request = project::get::Projects::builder()
         .page_size(50)
         .build()
         .unwrap();
 
+    // Send the request and deserialize the response
     let projects = client.get::<_, Vec<Project>>(request).await;
+
+    // Verify the response is correct
     assert!(projects.is_ok());
-    println!("{:?}", projects.unwrap());
 }
